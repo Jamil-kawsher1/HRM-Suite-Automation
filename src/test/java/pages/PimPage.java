1583536479b414c7bb4945aba7e25d81be5f0984
Jamil Kawsher <jamil15-9734@diu.edu.bd>
2023-02-25 14:50:28 +0600

    Add Empoyee Test case added

--- src/test/java/pages/PimPage.java
+++ src/test/java/pages/PimPage.java
@@ -13,15 +13,66 @@ public class PimPage {
     @FindBy(className = "oxd-main-menu-item-wrapper")
     List<WebElement> manueList;
 
+    //butoons
+    @FindBy(className = "oxd-button--secondary")
+    List<WebElement> buttons;
 
-    //page Factory intialization
+    //add employe first name
+    @FindBy(name = "firstName")
+    WebElement firstname;
+    //add employee  lastName
+    @FindBy(name = "lastName")
+    WebElement lastname;
+
+    //add employee page toggle button
+    @FindBy(className = "oxd-switch-input")
+    WebElement btnToggle;
+
+    // user login details all filed after toggle press
+    @FindBy(className = "oxd-input")
+    List<WebElement> loginDetailsFields;
+
+   //save and submit button after user  fill whole info
+    @FindBy(css = "[type=submit]")
+    WebElement employeeInfoSaveBtn;
+
+    //page Factory initialization
 
     public PimPage (WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
 
-    public void doClickOnPIM () {
+    public void doClickOnPIM () throws InterruptedException {
+        //clicking on PIM Section from dashboard
         manueList.get(1).click();
+//wating for 2 sec to load properly
+        Thread.sleep(2000);
 
+        //clicking on Ada employee button from PIM Section
+        buttons.get(1).click();
+
+    }
+
+
+    //add employee steps method
+
+    public void addEmployee () throws InterruptedException {
+//filling us first name last name filed
+        firstname.sendKeys("Test");
+        lastname.sendKeys("User");
+        //clicking on Toggle button to expand and create Login details
+        btnToggle.click();
+        Thread.sleep(1000);
+//username field
+        loginDetailsFields.get(5).sendKeys("test0777");
+//password filed
+        loginDetailsFields.get(6).sendKeys("Password@!1");
+//confirm password
+        loginDetailsFields.get(7).sendKeys("Password@!1");
+
+        Thread.sleep(1000);
+
+        //clicking on Save employee button
+        employeeInfoSaveBtn.click();
     }
 }
--- src/test/java/testRunner/PIMpageTestRunner.java
+++ src/test/java/testRunner/PIMpageTestRunner.java
@@ -1,7 +1,10 @@
 package testRunner;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.LoginPage;
 import pages.PimPage;
 import setup.Setup;
@@ -13,8 +16,26 @@ public class PIMpageTestRunner extends Setup {
         login.doLogin("Admin","admin123");
     }
 //    @Test(priority = 2,description = "Click PIM Click")
-    public void doClickOnPIM(){
+    public void doClickOnPIM() throws InterruptedException {
         PimPage pimpage=new PimPage(driver);
         pimpage.doClickOnPIM();
+        //watting for 3 sec
+        Thread.sleep(3000);
+        SoftAssert assret=new SoftAssert();
+        String messageActual=driver.findElement(By.className("orangehrm-main-title")).getText();
+        String messageExpected="Add Employee";
+        assret.assertTrue(messageActual.contains(messageExpected));
+
+    }
+
+
+
+    @Test(priority = 2,description = "add Employee ")
+    public void addEmployee() throws InterruptedException {
+PimPage pimPage=new PimPage(driver);
+pimPage.doClickOnPIM();
+Thread.sleep(2000);
+pimPage.addEmployee();
+
     }
 }
